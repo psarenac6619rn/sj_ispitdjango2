79a153760b1a6c0e63566f11beedf0c6879140f4
psarenac6619rn <psarenac6619rn@raf.rs>
2021-02-15 18:44:36 +0100

    Django gotovo

--- projekat/admin.py
+++ projekat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Stolica, Drvo, User
+
+admin.site.register(Stolica)
+admin.site.register(Drvo)
+admin.site.register(User)
